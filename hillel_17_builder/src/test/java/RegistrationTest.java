01db16c5c7957295d74fc05aaad66c6571dc3bf9
igorthedci <foumart@ukr.net>
2019-11-21 02:12:17 +0200

    Hillel 17 Homework. Builder (init)

--- hillel_17_builder/src/test/java/RegistrationTest.java
+++ hillel_17_builder/src/test/java/RegistrationTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class RegistrationTest {
+
+    private final String linkShop = "http://automationpractice.com";
+    private WebDriver driver;
+
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/index.php");
+    }
 }
